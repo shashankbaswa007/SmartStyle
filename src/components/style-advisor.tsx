ab837e1fe9217f18163f8acced5f2e62d19769dd
Shashank Baswa <baswashashank123@gmail.com>
2025-08-16 05:00:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/style-advisor.tsx
+++ src/components/style-advisor.tsx
@@ -12,6 +12,7 @@ import { generateOutfitImage } from "@/ai/flows/generate-outfit-image";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useToast } from "@/hooks/use-toast";
